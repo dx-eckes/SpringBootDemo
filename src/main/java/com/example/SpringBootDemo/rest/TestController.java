66b6178d1097ed12902cac0a8eccac91d937f9d8
无痕1083 <non_trace@163.com>
2019-01-14 18:33:48 +0800

    更新 TestController.java

--- src/main/java/com/example/SpringBootDemo/rest/TestController.java
+++ src/main/java/com/example/SpringBootDemo/rest/TestController.java
@@ -14,4 +14,9 @@ public class TestController {
         m.addAttribute("host","asdfasdfasdf");
         return "ok";
     }
+
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/swagger-ui.html";
+    }
 }
