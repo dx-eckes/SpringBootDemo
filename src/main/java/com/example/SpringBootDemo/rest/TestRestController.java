a7c157ed4bba8e70e702bdb724a4cae7f0a06aa9
无痕1083 <non_trace@163.com>
2019-01-14 18:15:29 +0800

    更新 TestRestController.java

--- src/main/java/com/example/SpringBootDemo/rest/TestRestController.java
+++ src/main/java/com/example/SpringBootDemo/rest/TestRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @Api(value = "测试API接口,rest接口1111111111",description = "测试接口描述")
 public class TestRestController {
 
-    @ApiOperation(value="测试rest接口", notes="测试rest接口，测试get接口")
+    @ApiOperation(value="测试rest接口tttttt", notes="测试rest接口，测试get接口")
     @ApiModelProperty
     @PostMapping(value = "v1/test")
     public String test(@ApiParam("字符串参数，用于返回") @RequestParam("testStr") String testStr, @ApiParam("日期参数，打印") @RequestParam("date") String date,@ApiParam("实体参数，打印") @RequestBody TestUser user){
